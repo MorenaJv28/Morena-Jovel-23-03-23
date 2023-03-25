65d5d14a5f2426982e634dbedd693a55f5fb3fbd
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-03-25 13:00:36 -0600

    actividad semana 8 y 9

---
+++ Almacen/Almacen/DAO/CrudProductos.cs
@@ -0,0 +1,27 @@
+﻿using Almacen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.DAO
+{
+    public class CrudProductos
+    {
+        public void AgregarProducto(Producto ParametroProducto)
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                Producto Pro = new Producto();
+
+                Pro.Nombre = ParametroProducto.Nombre;
+                Pro.Descripcion = ParametroProducto.Descripcion;
+                Pro.Precio = ParametroProducto.Precio;
+                Pro.Stock = ParametroProducto.Stock;
+                db.Add(Pro);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,64 @@
+﻿using Almacen.DAO;
+using Almacen.Models;
+
+CrudProductos CrudProductos = new CrudProductos();
+Producto Pro = new Producto();
+
+Console.Write(@"Menu
+Pulse 1 para Insertar un nuevo Producto
+Pulse 2 para Salir
+-> ");
+
+var Menu = Convert.ToInt32(Console.ReadLine());
+
+switch (Menu)
+{
+    case 1:
+        bool bucle = true;
+        while (bucle == true)
+        {
+            Console.Write("Ingrese el nombre del producto -> ");
+            Pro.Nombre = Console.ReadLine();
+            Console.Write("\nIngrese una breve descripcion del producto -> ");
+            Pro.Descripcion = Console.ReadLine();
+            Console.Write("\nIngrese el precio del producto -> $");
+            Pro.Precio = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("\nIngrese la cantidad de productos existentes");
+            Pro.Stock = Convert.ToInt32(Console.ReadLine());
+
+            CrudProductos.AgregarProducto(Pro);
+
+            Console.Write(@"
+El Producto se ingreso correctamente
+Pulsa 1 para seguir insertando Productos
+Pulsa 2 para salir
+-> ");
+
+            var Menu2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (Menu2)
+            {
+                case 1:
+                    bucle = true;
+                    break;
+
+                case 2:
+                    Console.Write("Gracias por utilizar el programa");
+                    bucle = false;
+                    break;
+
+                default:
+                    Console.Write("Ingrese una opcion del menu");
+                    break;
+            }
+        }
+        break;
+
+    case 2:
+        Console.Write("Gracias por utilizar el programa");
+        break;
+
+    default:
+        Console.Write("La opcion no se encuentra disponible");
+        break;
+}
